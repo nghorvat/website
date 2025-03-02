32c86d4858af5924126d9d005c5fdf757091ab1a
Nathan Horvath <ngh423@hotmail.com>
2025-03-01 19:12:25 -0800

    clean up function names and colour references

--- app.py
+++ app.py
@@ -10,48 +10,48 @@ github_url = 'https://github.com/nghorvat'
 twitter_url = 'https://x.com/DJSnuggz'
 linkedin_url = 'https://www.linkedin.com/in/nathan-horvath/'
 
-main_colour = '#E4A300'
+dark_yellow = '#E4A300'
 dark_grey = '#1A1A1A'
 light_grey = '#333333'
 muted_white = '#BDBDBD'
 extra_muted_white = '#8A8780'
 
-def _github_icon_link(h, w): return UkIconLink("github", height=h, width=w, href=github_url, cls="text-[#E4A300]")
-def _twitter_icon_link(h, w): return UkIconLink("twitter", height=h, width=w, href=twitter_url, cls='text-[#E4A300]')
-def _linkedin_icon_link(h, w): return UkIconLink("linkedin", height=h, width=w, href=linkedin_url, cls='text-[#E4A300]')
+def _github_icon_link(h, w): return UkIconLink("github", height=h, width=w, href=github_url, cls=f"text-[{dark_yellow}]")
+def _twitter_icon_link(h, w): return UkIconLink("twitter", height=h, width=w, href=twitter_url, cls=f"text-[{dark_yellow}]")
+def _linkedin_icon_link(h, w): return UkIconLink("linkedin", height=h, width=w, href=linkedin_url, cls=f"text-[{dark_yellow}]")
 
 hdrs = (
     Theme.yellow.headers(mode='dark')
 )
 
-def navbar():
-    return Div(cls='bg-[#1A1A1A] w-screen')(
+def _navbar():
+    return Div(cls=f'bg-[{dark_grey}] w-screen')(
             NavBar(
             DivLAligned(
-                A('About', href='/', cls=(TextPresets.bold_lg, 'text-[#E4A300]')),
-                A('Blog', href='/blog', cls=(TextPresets.bold_lg, 'text-[#E4A300]')),
+                A('About', href='/', cls=(TextPresets.bold_lg, f"text-[{dark_yellow}]")),
+                A('Blog', href='/blog', cls=(TextPresets.bold_lg, f"text-[{dark_yellow}]")),
                 _github_icon_link(24, 24),
                 _twitter_icon_link(24, 24),
                 _linkedin_icon_link(24, 24),
                 cls='space-x-6 mr-11'
             ),
-            brand=A(H2('Nathan Horvath', cls=('text-[#E4A300] ml-11', TextT.bold)), href="/")
+            brand=A(H2('Nathan Horvath', cls=(f'text-[{dark_yellow}] ml-11', TextT.bold)), href="/")
         )
     )
 
-def error_page(code_str, message_str, gif_path, txt_str):
+def _error_page(code_str, message_str, gif_path, txt_str):
     return Div(
-        navbar(),
+        _navbar(),
         DivCentered(cls='space-y-6 mt-12')(
-            H1(code_str, cls=('text-[#E4A300]', 'text-6xl')),
+            H1(code_str, cls=(f"text-[{dark_yellow}]", 'text-6xl')),
             H1(message_str),
             Img(src=gif_path, cls='w-3/4 max-w-screen-sm'),
-            P(txt_str, cls="text-[#BDBDBD] text-lg"),
-            A(Button("Go Home", cls=(ButtonT.primary, 'bg-[#e4A300]')), href="/")
+            P(txt_str, cls=f"text-[{muted_white}] text-lg"),
+            A(Button("Go Home", cls=(ButtonT.primary, f'bg-[{dark_yellow}]')), href="/")
         ))
 
-def not_found(req, exc): return error_page("404", "Page Not Found", "/static/larry-david-lost.gif", "The page you're looking for doesn't exist or has been moved.")
-def server_error(req, exc): return error_page("500", "Server Error", "/static/larry-david-sorry.gif", "Something went wrong on our server. Please try again later.")
+def not_found(req, exc): return _error_page("404", "Page Not Found", "/static/larry-david-lost.gif", "The page you're looking for doesn't exist or has been moved.")
+def server_error(req, exc): return _error_page("500", "Server Error", "/static/larry-david-sorry.gif", "Something went wrong on our server. Please try again later.")
 
 app, rt = fast_app(hdrs=hdrs, 
                    live=True, 
@@ -60,7 +60,7 @@ app, rt = fast_app(hdrs=hdrs,
                                        Exception: server_error}
                     )
 
-def _post(folder_name):
+def _post_data(folder_name):
     md_name = folder_name.ls(file_exts=['.md'])[0]
     _, meta, post = md_name.read_text().split('---\n')
     meta = yaml.safe_load(meta)
@@ -68,11 +68,11 @@ def _post(folder_name):
     meta['slug'] = slugify(meta['title'], replacements=[("'", "")])
     return meta, post
 
-posts = Path('posts').ls().map(_post).sorted(key=lambda p: date.fromisoformat(p[0]['date']), reverse=True)
+posts = Path('posts').ls().map(_post_data).sorted(key=lambda p: date.fromisoformat(p[0]['date']), reverse=True)
 
-def blog_card(post):
+def _blog_card(post):
 
-    def SmallLabel(text): return Label(text, cls='bg-[#333333]')
+    def SmallLabel(text): return Label(text, cls=f'bg-[{light_grey}]')
     def SmallTags(cats): return DivLAligned(map(SmallLabel, cats))
 
     meta = post[0]
@@ -82,40 +82,40 @@ def blog_card(post):
             A(Img(src=meta['image_path'], style="width:200px"),href=f'/blog/posts/{meta["slug"]}'),
             Div(cls='space-y-3 w-full')(
                 A(H3(meta["title"]), href=f'/blog/posts/{meta["slug"]}'),
-                P(meta["description"], cls="text-[#BDBDBD] text-lg"),
-                DivFullySpaced(map(Small, [meta["author"], meta["date"]]), cls="text-[#8A8780]"),
+                P(meta["description"], cls=f"text-[{muted_white}] text-lg"),
+                DivFullySpaced(map(Small, [meta["author"], meta["date"]]), cls=f"text-[{extra_muted_white}]"),
                 DivFullySpaced(
                     SmallTags(meta["categories"]),
-                    A(Button("Read",  cls=(ButtonT.primary, 'h-6', 'bg-[#e4A300]')), href=f'/blog/posts/{meta["slug"]}')))),
-        cls=(CardT.hover, "bg-[#1A1A1A] rounded-lg"))
+                    A(Button("Read",  cls=(ButtonT.primary, 'h-6', f'bg-[{dark_yellow}]')), href=f'/blog/posts/{meta["slug"]}')))),
+        cls=(CardT.hover, f"bg-[{dark_grey}] rounded-lg"))
 
-def author_pub_box(meta):
+def _author_pub_box(meta):
     return Container(
         DivFullySpaced(
             DivLAligned(
-                P('AUTHOR', cls=("text-[#8A8780] text-lg", 'mr-4', '-ml-10')),
+                P('AUTHOR', cls=(f"text-[{extra_muted_white}] text-lg", 'mr-4', '-ml-10')),
                 P(meta['author'], cls=TextT.lg)
             ),
             DivLAligned(
-                P('PUBLISHED', cls=("text-[#8A8780] text-lg", 'mr-4')),
+                P('PUBLISHED', cls=(f"text-[{extra_muted_white}] text-lg", 'mr-4')),
                 P(meta['date'], cls=TextT.lg)
             ),
             Div(),
             cls='p-6'
         ),
-        cls=(ContainerT.lg, 'bg-[#1A1A1A] rounded-lg')  # rounded corners on gray box
+        cls=(ContainerT.lg, f'bg-[{dark_grey}] rounded-lg')  # rounded corners on gray box
     )
 
-def blog_post(meta, content):
+def _blog_post(meta, content):
 
-    def LargeLabel(text): return Label(text, cls=(TextT.lg, 'bg-[#333333]'))
+    def LargeLabel(text): return Label(text, cls=(TextT.lg, f'bg-[{light_grey}]'))
     def LargeTags(cats): return DivLAligned(map(LargeLabel, cats))
 
     return Article(
         ArticleTitle(meta['title']),
-        P(meta['description'], cls="text-[#BDBDBD] text-lg"),
+        P(meta['description'], cls=f"text-[{muted_white}] text-lg"),
         LargeTags(meta['categories']),
-        author_pub_box(meta),
+        _author_pub_box(meta),
         DividerSplit(),
         render_md(content),
         cls='prose space-y-4'
@@ -124,12 +124,12 @@ def blog_post(meta, content):
 @rt("/blog")
 def blog_home():
     return Div(
-        navbar(),
+        _navbar(),
         Container(
             Div(
                 H1("Some Lessons Learned"),
                 DividerSplit(),
-                *[blog_card(post) for post in posts],
+                *[_blog_card(post) for post in posts],
                 cls='space-y-8 p-6'
             )
         )
@@ -138,7 +138,7 @@ def blog_home():
 @rt
 def index():
     return Div(
-        navbar(),
+        _navbar(),
         Container(
             DivCentered(
                 H1("About Me"),
@@ -150,13 +150,13 @@ def index():
                         DivCentered(
                             P("Subscribe to follow along", cls=TextT.bold),
                             DivHStacked(
-                                Input(placeholder="Your email", cls='bg-[#333333]'),
-                                Button("Join", cls=(ButtonT.primary, 'bg-[#e4A300]'))
+                                Input(placeholder="Your email", cls=f'bg-[{light_grey}]'),
+                                Button("Join", cls=(ButtonT.primary, f'bg-[{dark_yellow}]'))
                             ),
                             cls='space-y-4 p-4'
                     ),
                     cls=(ContainerT.lg, "rounded-lg"),
-                    style="background-color: #1A1A1A"
+                    style=f"background-color: {dark_grey}"
                 ),
                 cls='space-y-3 p-6'
             ),
@@ -179,9 +179,9 @@ def blog_post_page(slug: str):
         raise HTTPException(status_code=404, detail=f"Blog post with slug '{slug}' not found")
     post = posts[idx]
     return Div(
-        navbar(),
+        _navbar(),
         Div(
-            blog_post(*post),
+            _blog_post(*post),
             cls='p-14'
         )
     )
